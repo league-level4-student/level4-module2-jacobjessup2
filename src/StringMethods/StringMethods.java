69c2de5e5b1b2afecb62fced1b7294135f8831fc
monij <monij@DESKTOP-4MC4PHB.home>
2020-07-22 16:58:03 -0700

    Strings Methods(WIP)

--- src/StringMethods/StringMethods.java
+++ src/StringMethods/StringMethods.java
@@ -32,27 +32,61 @@ public class StringMethods {
 
 	// Given Strings s1 and s2, return the longer String
 	public static String longerString(String s1, String s2) {
-		return null;
+		if(s1.length()>s2.length()) {
+			return s1;
+		}
+		return s2;
 	}
-
+	
 	
 	// if String s contains the word "underscores", change all of the spaces to underscores
 	public static String formatSpaces(String s) {
-		return null;
-	}
+		String under = s;
+		if(s.indexOf("underscores")!= -1) {
+			under = under.replace(' ', '_');
+			return under;
+			}
+			return s;
+		}
+		
+		
+	
 
 	
 	// Return the name of the person whose LAST name would appear first if they were in alphabetical order
 	// You cannot assume there are no extra spaces around the name, but you can
 	// assume there is only one space between the first and last name
 	public static String lineLeader(String s1, String s2, String s3) {
+		String p1 = s1.trim();
+		String p2 = s2.trim();
+		String p3 = s3.trim();
+		String last1 = p1.charAt(p1.length()-1)+"";
+		String last2 = p2.charAt(p2.length()-1)+"";
+		String last3 = p3.charAt(p3.length()-1)+"";
+		if(last1.compareTo(last2)<0 && last1.compareTo(last3)<0) {
+			return p1;
+		}
+		else if(last2.compareTo(last1)<0 && last2.compareTo(last3)<0) {
+			return p2;
+		}
+		else if(last3.compareTo(last2)<0 && last3.compareTo(last1)<0) {
+			return p3;
+		}
 		return null;
 	}
 	
 	
 	// Return the sum of all numerical digits in the String
 	public static int numeralSum(String s) {
-		return 0;
+		int sum = 0;
+		char temp = ' ';
+		for (int i = 0; i < s.length(); i++) {
+			 temp = s.charAt(i);
+			if(Character.isDigit(temp) == true) {
+				sum += Character.getNumericValue(temp);
+			}
+		}
+		return sum;
 	}
 	
 	
